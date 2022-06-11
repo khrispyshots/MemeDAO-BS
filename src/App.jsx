3eb76b6552e6aa19f9ff41758361b473159b37c0
zainamroti <sayedzainjeelani@gmail.com>
2022-06-11 19:12:09 +0500

    Initialize 3web sdk

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ const App = () => {
   if(!address) {
     return (
       <div className="landing">
-        <h1>Welcome to NarutoDAO</h1>
+        <h1>Welcome to MemeDAO</h1>
         <button onClick={connectWithMetamask} className="btn-hero">
           Connect your wallet
         </button>
